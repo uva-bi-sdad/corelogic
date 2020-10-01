979b5dd3e9bbcfa19174827e8013ad3992efebfb
Aaron D. Schroeder <ads7fg@virginia.edu>
2020-10-01 10:20:56 -0400

    parallel insert

--- src/big files.R
+++ src/big files.R
@@ -1,6 +1,7 @@
 library(vroom)
 library(RPostgreSQL)
 library(magrittr)
+library(data.table)
 
 get_db_conn <-
   function(db_name = "sdad",
@@ -34,7 +35,15 @@ dbWriteTable(con, c("corelogic_usda", "corelogic_usda_deed_2020_06_27_17_18_19")
 # col_delim_num <- stringr::str_count(first_line, "\\|")
 # col_num <- col_delim_num + 1
 
-# deeds_all <- vroom("data/Corelogic_USDA_Deed_2020_06_27.txt")
+deeds_all <- vroom("/project/biocomplexity/sdad/projects_data/usda/bb/original/Corelogic_June_2020_Files/Corelogic_USDA_Deed_2020_06_27_10M.txt")
+
+deeds_all <- fread("/project/biocomplexity/sdad/projects_data/usda/bb/original/Corelogic_June_2020_Files/Corelogic_USDA_Deed_2020_06_27_50M.txt")
+
+deeds_cols <- fread("/project/biocomplexity/sdad/projects_data/usda/bb/original/Corelogic_June_2020_Files/Corelogic_USDA_Deed_2020_06_27_10M.txt", nrows = 1)
+
+colnames(deeds_all) <- colnames(deeds_cols)
+
+deeds_all[`RECORDING DATE (YYYYMMDD)` %like% "^2018", .N]
 
 db_col_names <- dataplumbr::name.standard_col_names(colnames(deeds_all)) %>%
   stringr::str_replace("^_", "") %>%
