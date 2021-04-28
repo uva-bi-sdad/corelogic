c72fed8b667aba6043a6a92bb39c451f1a8556c2
Aaron Schroeder <dads2busy@gmail.com>
2021-04-28 15:29:59 -0400

    final real estate sale characteristics table

--- src/county_export.R
+++ src/county_export.R
@@ -3,64 +3,41 @@ library(xlsx)
 library(zip)
 library(data.table)
 library(magrittr)
+source("R/functions.R")
 
 
-get_db_conn <-
-  function(db_name = "sdad",
-           db_host = "postgis1",
-           db_port = "5432",
-           db_user = Sys.getenv("db_usr"),
-           db_pass = Sys.getenv("db_pwd")) {
-    RPostgreSQL::dbConnect(
-      drv = RPostgreSQL::PostgreSQL(),
-      dbname = db_name,
-      host = db_host,
-      port = db_port,
-      user = db_user,
-      password = db_pass
-    )
-  }
-con <- get_db_conn()
+
+con <- get_db_conn(db_host = "localhost", db_port = 5434)
 
 counties <- merge(setDT(tigris::states()), setDT(tigris::counties()), by = "STATEFP") %>%
   .[, .(abbrv_st = STUSPS, name_co = NAMELSAD, geoid_co = GEOID.y)]
 
-counties <- counties[abbrv_st=="IN",]
+# counties <- counties[abbrv_st=="IN",]
 
 for (i in 1:nrow(counties)) {
   qry <-
     paste0("SELECT
     geoid_cnty,
+    geoid_blk,
     p_id_iris_frmtd,
-    sale_date,
-    sale_price,
-    sale_code,
-    transaction_type,
+    acres,
+    land_square_footage,
     bldg_code,
     building_square_feet,
     living_square_feet,
     year_built,
     effective_year_built,
     bedrooms,
-    full_baths,
-    \"1qtr_baths\",
-    \"3qtr_baths\",
-    half_baths,
-    total_baths,
-    property_indicator,
-    zoning,
-    acres,
-    land_square_footage,
-    property_centroid_longitude,
+    baths_appraised,
+    situs_address property_address,
+    sale_price,
+    sale_date,
+    sale_year,
+    transaction_type,
     property_centroid_latitude,
-    pri_cat_code,
-    \"BIP\",
-    \"CC\",
-    \"RC\",
-    \"TCF\",
-    \"TCI\"
+    property_centroid_longitude
   FROM
-    corelogic_usda.broadband_variables_tax_2020_06_27_unq_prog
+    corelogic_usda.current_tax_200627_latest_all_add_vars_add_progs_geom_blk
   WHERE geoid_cnty = '", counties[i,]$geoid_co,"'
     AND property_indicator='10'")
 
@@ -85,7 +62,7 @@ for (i in 1:nrow(counties)) {
   unlink(csv_path)
 }
 
-dbDisconnect(con)
+DBI::dbDisconnect(con)
 
 
 
